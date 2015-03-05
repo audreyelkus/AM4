db43e5ad61f35d0ca02695637b456e1a782910de
Susie Carovillano <scarovil@wellesley.edu>
2015-03-04 22:08:06 -0500

    figuring out Jsonp, also git tutorial in notes

--- AM4.js
+++ AM4.js
@@ -1,4 +1,29 @@
- console.log(gapi["auth"]);
+
+
+$.getJSON("https://spreadsheets.google.com/feeds/list/1035SQBywbuvWHoVof3G-VI0rapYJslaeYN5tTpNZq2M/od6/public/values?alt=json-in-script&gid=0&callback=?",
+    function (response){
+      console.log(response);
+      items = response.feed.entry;
+      if (response.feed) {
+        console.log(response.feed.entry.length);
+        processCourses(items);
+      }
+
+    });
+    
+  var cleanedCourses; // global variable
+    function processCourses(allCourses){     
+      // your code for getting the desirced information from each course
+  }
+
+
+
+
+
+
+
+
+console.log(gapi["auth"]);
 gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true},
       handleAuthResult);
 var resource = {
