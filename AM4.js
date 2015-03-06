fb54cacb72b50688c3ad9c456bb3070414b04e31
Susie Carovillano <scarovil@wellesley.edu>
2015-03-05 22:22:45 -0500

    adding search functionality

--- AM4.js
+++ AM4.js
@@ -41,9 +41,12 @@
     });
     
     // global variables
-    var coursesByDept;
-    var coursesByReq;
-    var coursesByTime;
+    var coursesByDept = {CS:"Computer Science CS", lastName:"Doe", age:50, eyeColor:"blue"};
+    var coursesByReq = {Cognition:[], HistoricalStudies: [], LanguageAndLit: [], MathModeling:[], NaturalScience:[],
+                       BasicQR: [], QROverlay: [], Religion: [], SBA: [], Art: [], FYW: []};
+    var coursesByLevel = new Object();
+    var coursesByTime = new Object();
+    var thingy;
 
     function processCourses(allCourses){  
       
@@ -56,6 +59,7 @@
         course.curEnroll = allCourses[i].gsx$currentenrollment.$t
 
         course.distReqs = allCourses[i].gsx$distributions.$t.replace("------------------------------\n", "");
+        
         course.meetTimes = allCourses[i].gsx$meetingtimes.$t
 
         course.seatsAvail = allCourses[i].gsx$seatsavailable.$t
@@ -63,8 +67,88 @@
         
         console.log(course);
         
+        var distRe = course.distReqs
+        
+        //Fill courses by Req
+        if (distRe.indexOf("Epistemology and Cognition") >= 0) {
+          coursesByReq["Cognition"].push(course);
+        }
+        if (distRe.indexOf("Historical Studies") >= 0) {
+          coursesByReq["HistoricalStudies"].push(course);
+        }
+        if (distRe.indexOf("Language and Literature") >= 0) {
+          coursesByReq["LanguageAndLit"].push(course);
+        }
+        if (distRe.indexOf("Mathematical Modeling") >= 0) {
+          coursesByReq["MathModeling"].push(course);
+        }
+        if (distRe.indexOf("Natural and Physical Science") >= 0) {
+          coursesByReq["NaturalScience"].push(course);
+        }
+        if (distRe.indexOf("Basic QR") >= 0) {
+          coursesByReq["BasicQR"].push(course);
+        }
+        if (distRe.indexOf("QR Overlay") >= 0) {
+          coursesByReq["QROverlay"].push(course);
+        }
+        if (distRe.indexOf("Religion, Ethics and Moral Philosophy") >= 0) {
+          coursesByReq["Religion"].push(course);
+        }
+        if (distRe.indexOf("Social and Behavioral Analysis") >= 0) {
+          coursesByReq["SBA"].push(course);
+        }
+        if (distRe.indexOf("Arts, Music, Theatre, Film and Video") >= 0) {
+          coursesByReq["Art"].push(course);
+        }
+        if (distRe.indexOf("First-Year Writing") >= 0) {
+          coursesByReq["FYW"].push(course);
+        }
+        
+        
+        var short = course.shortTitle;
+        
+        if (short.index("Epistemology and Cognition") >= 0) {
+          coursesByReq["Cognition"].push(course);
+        }
+        if (short.indexOf("Historical Studies") >= 0) {
+          coursesByReq["HistoricalStudies"].push(course);
+        }
+        if (short.indexOf("Language and Literature") >= 0) {
+          coursesByReq["LanguageAndLit"].push(course);
+        }
+        if (short.indexOf("Mathematical Modeling") >= 0) {
+          coursesByReq["MathModeling"].push(course);
+        }
+        if (short.indexOf("Natural and Physical Science") >= 0) {
+          coursesByReq["NaturalScience"].push(course);
+        }
+        if (short.indexOf("Basic QR") >= 0) {
+          coursesByReq["BasicQR"].push(course);
+        }
+        if (short.indexOf("QR Overlay") >= 0) {
+          coursesByReq["QROverlay"].push(course);
+        }
+        if (short.indexOf("Religion, Ethics and Moral Philosophy") >= 0) {
+          coursesByReq["Religion"].push(course);
+        }
+        if (short.indexOf("Social and Behavioral Analysis") >= 0) {
+          coursesByReq["SBA"].push(course);
+        }
+        if (short.indexOf("Arts, Music, Theatre, Film and Video") >= 0) {
+          coursesByReq["Art"].push(course);
+        }
+        if (short.indexOf("First-Year Writing") >= 0) {
+          coursesByReq["FYW"].push(course);
+        }
+        
+        
+        
+      
+              
         
       }
+      
+      console.log(coursesByReq);
       
     }
   
