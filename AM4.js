689b40e053719ad1d8c4bfc257149b9baae6fe34
Susie Carovillano <scarovil@wellesley.edu>
2015-03-07 15:42:54 -0500

    Set default values for the forms

--- AM4.js
+++ AM4.js
@@ -27,14 +27,6 @@
 
 
 
-// working on regex ok so here's her example
-
-
-
-
-
-
-
 // CODE FROM ENI    
 // this code should go in a separate Javascript file  
   $.getJSON("https://spreadsheets.google.com/feeds/list/1035SQBywbuvWHoVof3G-VI0rapYJslaeYN5tTpNZq2M/od6/public/values?alt=json-in-script&gid=0&callback=?",
@@ -48,27 +40,11 @@
 
     });
     
-    // global variables
-    var coursesByDept = {AFR: [], AMST:[], ANTH:[], ARAB:[], ARTH:[], ARTS: [], ASTR: [], BIOC: [], BISC: [],
-                         CAMS: [], CHEM:[], CHIN:[], CLCV:[], CLSC:[], CPLT:[], CS:[], ECON:[], EDUC: [],
-                         ENG: [], ES: [], EXTD: [], FREN:[], GEOS:[], GER:[], GRK:[], HEBR: [], HNUR: [],
-                         HIST: [], ITAS: [], JPN: [], KOR: [], LAT:[], LING:[], MER:[], MATH:[], MES:[], MUS:[],
-                         NEUR: [], PEAC: [], PHIL: [], PHYS: [], POL: [], POL1:[], POL2:[], POL3:[], POL4:[],
-                         PORT:[], PSYC:[], QR: [], REL:[], RUSS: [], SAS: [], SOC: [], SPAN: [], SUST:[],
-                         SWA:[], THST:[], WGST:[], WRIT:[]};
-    var coursesByReq = { "Epistemology and Cognition":[], "Historical Studies": [], "Language and Literature": [],
-                        "Mathematical Modeling":[], "Natural and Physical Science":[], "Basic QR": [], "QR Overlay": [],
-                        "Religion, Ethics and Moral Philosophy": [], "Social and Behavioral Analysis": [], 
-                        "Arts, Music, Theatre, Film and Video": []};
-    var coursesByLevel = new Object();
-    var coursesByTime = new Object();
+// global variables
+var cleanCourses = new Object();
 
-    var cleanCourses = new Object();
-    var thingy;
-
-    function processCourses(allCourses){  
-      
-      for (i in allCourses) {
+function processCourses(allCourses){  
+  for (i in allCourses) {
         
         var course = new Object();
         course.title = allCourses[i].gsx$title.$t;
@@ -97,17 +73,6 @@
       
     }
 
-
-        
-        
-        //when search by dept
-        //loop through crn looking for match for dept
-        //if second condition
-        //loop through subset array that matches 1st condition
-        
-  
-//from the course example
-
 $('#department').bind('click', search);
 $('#distributions').bind('click', search);
 
@@ -128,7 +93,8 @@ function search()
   console.log(distChoice);
   
   for(i in cleanCourses) {
-    if (cleanCourses[i].shortTitle.indexOf(deptChoice) >= 0) {
+    
+   if (cleanCourses[i].shortTitle.indexOf(deptChoice) >= 0) {
       results1.push(cleanCourses[i]);
     }
     
